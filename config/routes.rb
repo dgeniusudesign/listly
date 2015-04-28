3378ffc7a8b94a55fa7639d47128993891cc9dc6
mk <dgeniusudesign@gmail.com>
2015-04-27 22:29:50 -0500

    Sign in completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :lists
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'static_pages#index'
+  authenticated :user do
+    root to: "home#index", as: "home"
+  end
+
+  unauthenticated :user do
+    root 'welcome#index'
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
